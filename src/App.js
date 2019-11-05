b8f8e532e503822fd5997fc454a9e972975a0232
Rodrigo Reis <roreis99@gmail.com>
2019-11-05 15:19:41 -0300

    feat: config routes to newuser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return <h1>Ola</h1>;
-}
+import Login from './pages/Login';
+import NewUser from './pages/NewUser';
+
+const App = () => (
+  <Router>
+    <Route exact path="/" component={Login} />
+    <Route exact path="novo-usario" component={NewUser} />
+  </Router>
+);
 
 export default App;
